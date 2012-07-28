e13600745cb09d7fd019366d622e0493a36219c5
indispect <amit.ashkenazi77@gmail.com>
2012-07-28 19:31:46 +0300

    set route

--- config/routes.rb
+++ config/routes.rb
@@ -56,6 +56,8 @@ Indispect::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => 'home#index'
+  
+  match 'home/set_fb_user' => 'home#set_fb_user'
 
   # See how all your routes lay out with "rake routes"
 
