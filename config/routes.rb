e5dea3b9eded53a8b6d71b0ae6a681ddc34b181c
indispect <amit.ashkenazi77@gmail.com>
2012-07-30 09:54:37 +0300

    db migrate change routing

--- config/routes.rb
+++ config/routes.rb
@@ -55,7 +55,7 @@ Indispect::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
+  root :to => 'user#index'
   
   match '/set_fb_user' => 'home#set_fb_user'
 
