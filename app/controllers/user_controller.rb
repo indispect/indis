fad4cd9319ddeb664ce1b519cf6132e42e94d5ae
unknown <amit@amit-HP.(none)>
2012-08-25 13:45:45 +0300

    debug

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -14,6 +14,7 @@ class UserController < ApplicationController
     puts "In Login"
     user = User.where('app_id = ? AND secret = ?',params[:appId],params[:secret]).limit(1)
     if user
+      puts user.to_s + "  user <<<<<<<<<<"
       cookies[:user] = user.email  
       indi_token = session[:indi_token] = SecureRandom.base64(10)
       render :json => indi_token
