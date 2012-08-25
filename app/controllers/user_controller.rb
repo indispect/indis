51d64fb532a449a3a71c3e7e27470fe486ffd1b1
unknown <amit@amit-HP.(none)>
2012-08-25 13:42:00 +0300

    debug

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -9,17 +9,11 @@ class UserController < ApplicationController
     render "users/account/index"
   end
   
-  def authenticate
-    puts params[:appId].to_s + " app id " + params[:secret] + " secret !!!!"
-    if User.where('app_id = ? AND secret = ?',params[:appId],params[:secret]).limit(1)
-      return true
-    end
-  end
-  
   def login
     
     puts "In Login"
-    if user = User.authenticate
+    user = User.where('app_id = ? AND secret = ?',params[:appId],params[:secret]).limit(1)
+    if user
       cookies[:user] = user.email  
       indi_token = session[:indi_token] = SecureRandom.base64(10)
       render :json => indi_token
