00a860fc9966329a05a667c63fc2d516946d3182
unknown <amit@amit-HP.(none)>
2012-08-25 13:58:03 +0300

    debug

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -12,7 +12,7 @@ class UserController < ApplicationController
   def login
     
     puts params[:appId].to_s + " <<<<<  "+ params[:secret].to_s + " In Login"
-    user = User.where('app_id = ? AND secret = ?',params[:appId],params[:secret]).limit(1)
+    user = User.where('app_id = ? AND secret = ?',params[:appId],params[:secret])
     if user
       puts user.to_s + "  user <<<<<<<<<<"
       cookies[:user] = user.email  
