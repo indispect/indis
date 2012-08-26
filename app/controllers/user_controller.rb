6dbb282d8fda62cf502907ec3bca7431b3c74010
unknown <amit@amit-HP.(none)>
2012-08-26 22:30:21 +0300

    debug

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -17,7 +17,7 @@ class UserController < ApplicationController
     puts params[:appId].to_s + " <<<<<  "+ params[:secret].to_s + " In Login xxx"
     
     
-    @user = User.where('app_id = ? AND secret = ?',params[:appId],params[:secret])
+    @user = User.where('app_id = ? AND secret = ?',params[:appId].to_s,params[:secret].to_s)
     if @user
       puts @user.to_s + "  user <<<<<<<<<<"
       cookies[:user] = @user.email  
